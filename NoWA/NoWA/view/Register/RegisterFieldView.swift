bf57a08a41643a782c55059de5a3c7cb37946352
Ernesto Garmendia <Ernesto@Ernestos-MacBook-Pro.local>
2015-12-01 12:22:14 -0300

    Agrego botones a pantalla de login

--- NoWA/NoWA/view/Register/RegisterFieldView.swift
+++ NoWA/NoWA/view/Register/RegisterFieldView.swift
@@ -7,7 +7,7 @@
 //
 
 class RegisterFieldView: UIView {
-
+    
     var iconImage : UIImageView!
     var registerTextField : UITextField!
     var recoverlabel : UILabel!
@@ -16,15 +16,26 @@ class RegisterFieldView: UIView {
     override init(frame:CGRect) {
         super.init(frame:frame)
         
+        registerTextField = UITextField()
+        registerTextField.textColor = UIColor.whiteColor()
+        registerTextField.backgroundColor = UIColor.blueColor()
+        registerTextField.adjustsFontSizeToFitWidth = true
+        
+        self.addSubview(registerTextField)
+        
         recoverlabel = UILabel()
-        recoverlabel.textColor = UIColor.blackColor()
+        recoverlabel.textColor = UIColor.whiteColor()
         recoverlabel.backgroundColor = UIColor.yellowColor()
-        recoverlabel.font = UIFont(name: "HelveticaNeue", size: 15)
+        recoverlabel.font = UIFont(name: "HelveticaNeue", size: 12)
         recoverlabel.adjustsFontSizeToFitWidth = true
         recoverlabel.numberOfLines = 1
-        recoverlabel.text = "prueba"
+        recoverlabel.text = "RECUPERAR"
+        recoverlabel.hidden = true
         self.addSubview(recoverlabel)
         
+        lineView = UIView()
+        lineView.backgroundColor = UIColor.whiteColor()
+        self.addSubview(lineView)
         
     }
     
@@ -33,15 +44,42 @@ class RegisterFieldView: UIView {
         
     }
     
-    func setupConstrains(){
-        recoverlabel.autoPinEdge(ALEdge.Left, toEdge: .Left, ofView: self, withOffset: 40)
-        recoverlabel.autoPinEdge(ALEdge.Right, toEdge: .Right, ofView: self, withOffset: 10)
+    override func layoutSubviews() {
+        
+        
+        registerTextField.autoPinEdge(ALEdge.Left, toEdge: .Left, ofView: self, withOffset: 40)
+        registerTextField.autoPinEdge(ALEdge.Top, toEdge: .Top, ofView: self)
+        registerTextField.autoPinEdge(ALEdge.Bottom, toEdge: .Top, ofView: lineView)
+        
+        recoverlabel.autoPinEdge(ALEdge.Left, toEdge: .Right, ofView: registerTextField)
+        recoverlabel.autoPinEdge(ALEdge.Right, toEdge: .Right, ofView: self)
         recoverlabel.autoPinEdge(ALEdge.Top, toEdge: .Top, ofView: self)
-        recoverlabel.autoPinEdge(ALEdge.Bottom, toEdge: .Bottom, ofView: self)
-
+        recoverlabel.autoPinEdge(ALEdge.Bottom, toEdge: .Top, ofView: lineView)
+        recoverlabel.autoSetDimension(ALDimension.Width, toSize: self.frame.size.width / 3.5)
+        
+        lineView.autoPinEdge(ALEdge.Left, toEdge: .Left, ofView: self)
+        lineView.autoPinEdge(ALEdge.Right, toEdge: .Right, ofView: self, withOffset: 10)
+        lineView.autoPinEdge(ALEdge.Bottom, toEdge: .Bottom, ofView: self)
+        lineView.autoSetDimension(ALDimension.Height, toSize: 1.5)
+        
+        
     }
     
-    func setItems(){
+    func setItems(icon: String?, placeholder: String?, recover: Bool){
+        
+        //        if let _icon = icon{
+        //
+        //        }
+        
+        if let _placeholder = placeholder{
+            registerTextField.attributedPlaceholder =  NSAttributedString(string:_placeholder,
+                attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+        }
+        
+        if recover == true{
+            recoverlabel.hidden = false
+        }
+        
         
     }
     
--- NoWA/NoWA/viewcontroller/Register/RegisterViewController.swift
+++ NoWA/NoWA/viewcontroller/Register/RegisterViewController.swift
@@ -9,6 +9,8 @@
 class RegisterViewController: UIViewController {
     
     var emailView : RegisterFieldView!
+    var passwordView : RegisterFieldView!
+
     var aceptTermsButton : UIButton!
     var aceptTermsLabel : UILabel!
     var ingresarButton : UIButton!
@@ -21,16 +23,34 @@ class RegisterViewController: UIViewController {
         self.view.backgroundColor = .redColor()
         
         emailView = RegisterFieldView()
-        emailView.backgroundColor = .greenColor()
-        
-        emailView.setupConstrains()
+        emailView.backgroundColor = .grayColor()
+        emailView.setItems(nil,placeholder: "TU CORREO", recover: false)
         self.view.addSubview(emailView)
         
-
+        passwordView = RegisterFieldView()
+        passwordView.backgroundColor = .orangeColor()
+        passwordView.setItems(nil,placeholder: "CONTRASENIA", recover: true)
+        self.view.addSubview(passwordView)
+        
+        ingresarButton = UIButton()
+        ingresarButton.backgroundColor = .blueColor()
+        ingresarButton.setTitle("INGRESAR", forState: UIControlState.Normal)
+        ingresarButton.addTarget(self, action: "startApp", forControlEvents: UIControlEvents.TouchUpInside)
+        ingresarButton.layer.cornerRadius = 20
+        self.view.addSubview(ingresarButton)
+        
+        ingresarFacebookButton = UIButton()
+        ingresarFacebookButton.backgroundColor = .blueColor()
+        ingresarFacebookButton.setTitle("INGRESAR CON FACEBOOK", forState: UIControlState.Normal)
+        ingresarFacebookButton.addTarget(self, action: "loginFacebook", forControlEvents: UIControlEvents.TouchUpInside)
+        ingresarFacebookButton.layer.cornerRadius = 20
+        self.view.addSubview(ingresarFacebookButton)
+        
         setupConstrains()
         
-        
     }
+    
+    
     
     func setupConstrains(){
     
@@ -39,6 +59,29 @@ class RegisterViewController: UIViewController {
         emailView.autoPinEdge(ALEdge.Right, toEdge: .Right, ofView: self.view, withOffset: -40)
         emailView.autoPinEdge(ALEdge.Top, toEdge: .Top, ofView: self.view, withOffset: 40 )
     
+        passwordView.autoSetDimension(ALDimension.Height, toSize: 60)
+        passwordView.autoPinEdge(ALEdge.Left, toEdge: .Left, ofView: self.view, withOffset: 40)
+        passwordView.autoPinEdge(ALEdge.Right, toEdge: .Right, ofView: self.view, withOffset: -40)
+        passwordView.autoPinEdge(ALEdge.Top, toEdge: .Bottom, ofView: emailView, withOffset: 40 )
+    
+        ingresarButton.autoSetDimension(ALDimension.Height, toSize: 40)
+        ingresarButton.autoPinEdge(ALEdge.Left, toEdge: .Left, ofView: self.view, withOffset: 40)
+        ingresarButton.autoPinEdge(ALEdge.Right, toEdge: .Right, ofView: self.view, withOffset: -40)
+        ingresarButton.autoPinEdge(ALEdge.Top, toEdge: .Bottom, ofView: passwordView, withOffset: 50)
+        
+        ingresarFacebookButton.autoSetDimension(ALDimension.Height, toSize: 40)
+        ingresarFacebookButton.autoPinEdge(ALEdge.Left, toEdge: .Left, ofView: self.view, withOffset: 40)
+        ingresarFacebookButton.autoPinEdge(ALEdge.Right, toEdge: .Right, ofView: self.view, withOffset: -40)
+        ingresarFacebookButton.autoPinEdge(ALEdge.Top, toEdge: .Bottom, ofView: ingresarButton, withOffset: 50)
+        
+    }
+    
+    func startApp(){
+    
+    }
+    
+    func loginFacebook(){
+        
     }
     
 }
