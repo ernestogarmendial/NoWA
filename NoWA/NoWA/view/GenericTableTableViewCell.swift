de99d82e3d2d7cf04bd1e608062c33fb0bc147b9
Ernesto Garmendia <Ernesto@Ernestos-MacBook-Pro.local>
2015-12-10 17:14:34 -0300

    Se agregan clases de tablas para tabbar

---
+++ NoWA/NoWA/view/GenericTableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GenericTableTableViewCell.swift
+//  NoWA
+//
+//  Created by Ernesto Garmendia on 12/10/15.
+//  Copyright © 2015 Ernesto Garmendia Luis. All rights reserved.
+//
+
+import UIKit
+
+class GenericTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
