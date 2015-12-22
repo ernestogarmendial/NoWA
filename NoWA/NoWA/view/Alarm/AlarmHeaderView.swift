cd4f49940677172b24cde48b46022dc7a98e6a44
Ernesto Garmendia <Ernesto@MacBook-Pro-de-Ernesto.local>
2015-12-22 01:23:23 -0300

    Ajustes de diseño en torneos y alarmas

---
+++ NoWA/NoWA/view/Alarm/AlarmHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  AlarmHeaderView.swift
+//  NoWA
+//
+//  Created by Ernesto Garmendia Luis on 22/12/15.
+//  Copyright © 2015 Ernesto Garmendia Luis. All rights reserved.
+//
+
+import UIKit
+
+class AlarmHeaderView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NoWA/NoWA/view/Torneos/TorneoItemTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TorneoItemTableViewCell.swift
+//  NoWA
+//
+//  Created by Ernesto Garmendia Luis on 22/12/15.
+//  Copyright © 2015 Ernesto Garmendia Luis. All rights reserved.
+//
+
+import UIKit
+
+class TorneoItemTableViewCell: AlarmItemTableViewCell {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
