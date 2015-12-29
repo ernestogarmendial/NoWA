2558848dbce05e8cb77cc2a6ece05c1e7480bece
Ernesto Garmendia <Ernesto@MacBook-Pro-de-Ernesto.local>
2015-12-29 09:22:55 -0300

    trato de pegarle a servicio de forecasts

---
+++ NoWA/NoWA/model/dto/ForecastDTO.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastDTO.swift
+//  NoWA
+//
+//  Created by Ernesto Garmendia Luis on 29/12/15.
+//  Copyright © 2015 Ernesto Garmendia Luis. All rights reserved.
+//
+
+import UIKit
+
+class ForecastDTO: GenericDTO {
+
+}
---
+++ NoWA/NoWA/service/WeatherService.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherService.swift
+//  NoWA
+//
+//  Created by Ernesto Garmendia Luis on 28/12/15.
+//  Copyright © 2015 Ernesto Garmendia Luis. All rights reserved.
+//
+
+import UIKit
+
+class WeatherService: GenericService {
+
+}
