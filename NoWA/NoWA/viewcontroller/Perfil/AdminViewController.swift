d9d06317aa941a926a507924541bbb00f35c634a
Ernesto Garmendia <Ernesto@MacBook-Pro-de-Ernesto.local>
2016-02-07 01:21:59 -0300

    7. boton back en admin tiene la flechita original, pero es muy chica la imagen

--- NoWA/NoWA/viewcontroller/Perfil/AdminViewController.swift
+++ NoWA/NoWA/viewcontroller/Perfil/AdminViewController.swift
@@ -42,6 +42,28 @@ class AdminViewController: GenericViewController, UITableViewDelegate, UITableVi
         
     }
     
+    override func viewWillAppear(animated: Bool) {
+//        super.viewWillAppear(true)
+//        let myBackButton:UIButton = UIButton(type: .Custom) as UIButton
+//        myBackButton.addTarget(self, action: "popToRoot:", forControlEvents: UIControlEvents.TouchUpInside)
+//        myBackButton.setTitle("volver", forState: UIControlState.Normal)
+//        myBackButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+//        myBackButton.sizeToFit()
+//        let myCustomBackButtonItem:UIBarButtonItem = UIBarButtonItem(customView: myBackButton)
+//        self.navigationItem.leftBarButtonItem  = myCustomBackButtonItem
+        
+        let button = UIButton(type: .Custom) as UIButton
+        button.setImage(UIImage(named: "left_arrow"), forState: UIControlState.Normal)
+        button.addTarget(self, action: "popToRoot:", forControlEvents: UIControlEvents.TouchUpInside)
+        button.frame=CGRectMake(0, 0, 30, 30)
+        let barButton = UIBarButtonItem(customView: button)
+        self.navigationItem.leftBarButtonItem = barButton
+    }
+    
+    func popToRoot(sender:UIBarButtonItem){
+        self.navigationController!.popToRootViewControllerAnimated(true)
+    }
+    
     func callService(){
         let tournamentService : TournamentService = TournamentService()
         tournamentService.getTournaments(token: UserService.currentUser.token,target: self,message: "getTournamentsFinish:")
